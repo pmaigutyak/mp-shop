b8f56089be03e31cf447d2ddf0f7f00626f2dfe8
Yevgen Plyusnin <jplyusnin@gmail.com>
2017-03-20 20:52:44 +0200

    Update setup files.

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
+from shop import __version__
 
 setup(
     name='django-mp-shop',
-    version='1.0',
+    version=__version__,
     description='Django shop apps',
     long_description=open('README.md').read(),
     author='Paul Maigutyak',
     author_email='pmaigutyak@gmail.com',
     url='https://github.com/pmaigutyak/mp-shop',
-    download_url='https://github.com/pmaigutyak/mp-shop/archive/1.0.tar.gz',
-    packages=['shop'],
+    download_url='https://github.com/pmaigutyak/mp-shop/archive/%s.tar.gz' % __version__,
+    packages=find_packages(),
     license='MIT',
     install_requires=[]
 )
