aa0e62f345c26857576b8da97171e326cab0a166
Paul Maigutyak <pmaigutyak@ettera.com>
2017-03-21 09:12:19 +0200

    Fix package data including.

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@ from setuptools import setup, find_packages
 
 from shop import __version__
 
+
+with open('requirements.txt') as f:
+    requires = f.read().splitlines()
+
+
+url = 'https://github.com/pmaigutyak/mp-shop'
+
+
 setup(
     name='django-mp-shop',
     version=__version__,
@@ -10,11 +18,10 @@ setup(
     long_description=open('README.md').read(),
     author='Paul Maigutyak',
     author_email='pmaigutyak@gmail.com',
-    url='https://github.com/pmaigutyak/mp-shop',
-    download_url='https://github.com/pmaigutyak/mp-shop/archive/%s.tar.gz' % __version__,
+    url=url,
+    download_url='%s/archive/%s.tar.gz' % (url, __version__),
     packages=find_packages(),
-    package_data={'shop': ['*']},
     include_package_data=True,
     license='MIT',
-    install_requires=[]
+    install_requires=requires
 )
