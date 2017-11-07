971ed069a3b9282d033f88f157922b84afc3c7e9
jplyusnin <jplyusnin@gmail.com>
2017-11-07 13:56:24 +0200

    Add categories async loading

--- shop/products/urls.py
+++ shop/products/urls.py
@@ -1,11 +1,13 @@
 
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.views.generic import TemplateView
 
 from shop.products import views
 
 
 urlpatterns = [
+
+    url(r'^api/', include('shop.products.api.urls', namespace='api')),
 
     url(r'^$',
         TemplateView.as_view(template_name='products/categories.html'),
