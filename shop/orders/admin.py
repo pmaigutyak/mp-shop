96f4325abe256e432e48208845cfe424cf723f2c
Paul Maigutyak <pmaigutyak@ettera.com>
2018-03-27 16:21:14 +0300

    Update orders.

--- shop/orders/admin.py
+++ shop/orders/admin.py
@@ -3,18 +3,48 @@ from django.apps import apps
 from django.conf import settings
 from django import forms
 from django.contrib import admin
+from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
 
+from easy_select2.widgets import Select2
+
 from shop.lib import get_show_on_site_link
+from shop.currencies.lib import format_printable_price
 
 
-class OrderProductInline(admin.TabularInline):
+def summarize_orders_action(modeladmin, request, queryset):
 
-    model = apps.get_model('orders', 'OrderProduct')
-    extra = 0
+    total_price = 0
+    total_qty = 0
+
+    for order in queryset:
+        total_price += order.default_total
+        total_qty += order.product_count
+
+    total = _('Orders: {}, Products: {}, Sum: {}')
+
+    messages.success(request, total.format(
+        len(queryset),
+        total_qty,
+        format_printable_price(total_price).decode('utf-8')
+    ))
+
+summarize_orders_action.short_description = _('Summarize')
 
 
 class OrderProductForm(forms.ModelForm):
+
+    def clean(self):
+
+        data = self.cleaned_data
+
+        price = data.get('price')
+        parent = data.get('parent')
+
+        if parent and not price:
+            data['price'] = parent.price.default
+
+        return data
 
     def clean_title(self):
         data = self.cleaned_data
@@ -36,9 +66,6 @@ class OrderProductForm(forms.ModelForm):
         price = data.get('price')
         parent = data.get('parent')
 
-        if parent and not price:
-            price = parent.price
-
         if not parent and not price:
             raise forms.ValidationError(_('Select product or enter price'))
 
@@ -46,10 +73,19 @@ class OrderProductForm(forms.ModelForm):
 
     class Meta:
         model = apps.get_model('orders', 'OrderProduct')
+        widgets = {'parent': Select2}
         fields = '__all__'
 
 
+class OrderProductInline(admin.TabularInline):
+    form = OrderProductForm
+    model = apps.get_model('orders', 'OrderProduct')
+    extra = 0
+
+
 class OrderAdmin(admin.ModelAdmin):
+
+    actions = [summarize_orders_action]
 
     inlines = [OrderProductInline]
 
