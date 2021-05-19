987310bab93dd646bb6ae08eaaa7ae1e28ac4218
pmaigutyak <pmaigutyak@gmail.com>
2021-05-20 00:53:51 +0300

    Update form fields.

--- attributes/fields.py
+++ attributes/fields.py
@@ -1,27 +1,13 @@
 
-from django.forms import Field, ValidationError
-from django.utils.translation import ugettext_lazy as _
+from basement.fields import FormField
 
 from attributes.widgets import AttributesFormFieldWidget
 from attributes.forms import AttributesForm
 
 
-class AttributesFormField(Field):
+class AttributesFormField(FormField):
 
     widget = AttributesFormFieldWidget
-    form = None
 
-    def init_form(self, *args, **kwargs):
-
-        self.form = AttributesForm(*args, **kwargs)
-        self.widget.form = self.form
-
-    def clean(self, *args, **kwargs):
-
-        if not self.form.is_valid():
-            raise ValidationError(_('Form is invalid.'))
-
-        return self.form.cleaned_data
-
-    def commit(self, *args, **kwargs):
-        return self.form.commit(*args, **kwargs)
+    def _build_form(self, *args, **kwargs):
+        return AttributesForm(*args, **kwargs)
---
+++ product_images/fields.py
@@ -0,0 +1,10 @@
+
+from basement.images import fields
+
+from product_images.models import ProductImage
+
+
+class ImagesFormField(fields.ImagesFormField):
+
+    def __init__(self):
+        super().__init__(ProductImage)
